f26af372c4acc63aa007699b5b8f5dab5d7e0a07
Mandeep-codes-jtg <mandeep.kumar@joshtechnologygroup.com>
2025-07-22 12:56:15 +0530

    MK-RU-03 fix: review fixes

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,10 +1,12 @@
 import Splide from '@splidejs/splide'
 
-const splide = new Splide('.splide', {
-  perPage: 3,
-  rewind: true,
-  gap: '33px',
-  type: 'loop'
-})
-
-splide.mount()
+const root = document.querySelector('.splide')
+if (root) {
+  const splide = new Splide(root, {
+    perPage: 3,
+    rewind: true,
+    gap: '33px',
+    type: 'loop'
+  })
+  splide.mount()
+}
